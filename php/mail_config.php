0c5afccaf46ee9bbcf28b44736536b4805be0dfd
Catuz <ricardoabatantonio@gmail.com>
2026-04-18 00:14:33 +0800

    Update mail_config.php

--- php/mail_config.php
+++ php/mail_config.php
@@ -4,9 +4,9 @@ return [
     'host' => 'smtp.gmail.com',
     'port' => 587,
     'secure' => 'tls', // tls or ssl
-    'username' => 'dumact360@gmail.com',
-    'password' => 'bnytljybdpnqmyco',
-    'from_email' => 'dumact360@gmail.com',
+    'username' => 'email',
+    'password' => 'email app pasword',
+    'from_email' => 'email',
     'from_name' => 'D.H AZADA TIRE SUPPLY',
     'timeout' => 20
 ];
